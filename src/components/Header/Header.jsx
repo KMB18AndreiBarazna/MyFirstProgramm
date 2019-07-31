b4abbd0d3faaba86103ec241ea2acffa634c5f4e
lenovo <2andrewbor@gmail.com>
2019-07-31 15:34:51 +0300

    Первая проба коммита

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
     return <header className={s.header}>
         <div className={s.titul}>
             <img src='http://kacharent.ru/images/stories/logotip/logo_cacharent.jpg' className={s.img}/>
-            <span>По острову! - Каждому!!! ;-)</span>
+            <span>По острову! - Каждому!!! :-)</span>
         </div>
         <div className={s.loginBlock}>
             {props.isAuth
